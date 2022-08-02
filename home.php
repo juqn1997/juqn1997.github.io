9de17e254d97ef4f214be0cda00c48e2dee372c1
Bebeto1997 <110433962+Bebeto1997@users.noreply.github.com>
2022-08-01 19:49:54 -0500

    creando  mi primer sitio web

---
+++ home.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<body background ="fondolegal.jpg">
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="doj.jfif">
+  
+    <title>home</title>
+    <link rel="stylesheet" type="text/css" href="pro.css">
+</head>
+<body>
+<h1>BIENVENIDOS🍂</h1>
+<div class="pregunta">¿Quieres tener el jardin de tus sueños?</div>
+<br>
+<div class="primer_texto">Recomendacones para tener una hermosa jardineria:</div>
+<br> 1. Conseguir un suelo sano y libre de químicos
+Lo más importante de un jardín es el suelo. Un suelo bien nutrido, vivo y libre de químicos es la base de la salud de las plantas. Los fertilizantes y los fitosanitarios son los causantes de matar a los microorganismos beneficiosos que existen en el entorno. Los fertilizantes provocan que la planta esté nutricionalmente descompensada y sea propensa a ataques de plagas y a daños por frío o calor. También crean formas insolubles en el suelo y contaminan las aguas.
+<br>
+2. Cambiar los abonos químicos por algo natural
+El aporte de abonos químicos en el jardín conlleva problemas de desequilibrio nutricional. Estos problemas hacen que las plantas sean más propensas a plagas y enfermedades. Por ello, el mejor método para evitar estos problemas es utilizar microorganismos que aportan nitrógeno y otros nutrientes necesarios para que las plantas estén verdes y sanas.
+<br>
+3. Nutrir las plantas
+Para que las plantas puedan desarrollarse en un suelo sano y libre de patógenos, se debe aportar productos que confieran al suelo los nutrientes. Una estructura con microorganismos aumenta la captación de nutrientes por la planta, obteniendo mayor porte y vigor.
+<br>
+4. Aportar materia orgánica
+El aporte de materia orgánica al suelo es fundamental. Se favorece el crecimiento de las plantas en un suelo sano y libre de patógenos, capaz de que se instalen microorganismos y fauna beneficiosa. La mejor forma de hacerlo es aportar al suelo humus de lombriz, que además de aportar materia orgánica al suelo, mejora la estructura y aporta nutrientes a las plantas.
+
+<br> 
+5. Eliminar hojas secas
+<br>
+Al eliminar las hojas secas del jardín y de las plantas obtendrás un aspecto más verde y cuidado. Además, cuando se quitan las hojas muertas se evitan que sea el refugio de plagas y el lugar donde se pueden instalar hongos oportunistas que puedan afectar a nuestro jardín.
+<br>
+6. Podar las partes secas de las plantas
+Si se poda las partes que se encuentran muertas o en mal estado se favorece el crecimiento y se consigue que las plantas obtengan un mejor aspecto. Con ello se favorece la recuperación y el crecimiento de la planta, además de evitar enfermedades fúngicas en la planta afectada.
+<br>
+<br><br>   
+    <br>
+<img src="domo.jpg" height="250" width="250" align="center" >  
+<img src="doj.jfif" height="250" width="250" align="right" >  
+<br>
+<a href="pag2.html"><input type="image" src="adelante.png" height="80" width="80" align="right"></a> 
+<br>
+</body>
+</html>
